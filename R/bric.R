e178b6d165b20643356ae2e04d474b6313fd1685
zy26 <zy26@users.noreply.github.com>
2019-02-25 10:36:28 -0500

    Add main function for R

---
+++ R/bric.R
@@ -0,0 +1,15 @@
+#' @describeIn QUBIC Performs a QUalitative BIClustering.
+#'
+#' @usage qubic(i, R = FALSE, F = FALSE, d = FALSE, f = 0.85, k = 13, c = 0.90, o = 5000)
+qubic <- function(i, R = FALSE, F = FALSE, d = FALSE, f = 0.85, k = 13, c = 0.90, o = 5000) {
+  vec <- c("./qubic", "-i", i)
+  if(R) vec <- c(vec, "-R")
+  if(F) vec <- c(vec, "-F") 
+  if(d) vec <- c(vec, "-d") 
+  vec <- c(vec, "-f", as.character(f))
+  vec <- c(vec, "-k", as.character(k))
+  vec <- c(vec, "-c", as.character(c))
+  vec <- c(vec, "-o", as.character(o))
+  
+  return(.main(vec))
+}
