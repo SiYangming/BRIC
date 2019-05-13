ea1c6982fd29eb277079bbe5cb6f77dd7a356a24
Juan <juanxie.84@gmail.com>
2019-05-13 12:17:14 -0500

    add argument -n
    when n=TRUE, we can call discretize_new()

--- R/bric.R
+++ R/bric.R
@@ -4,13 +4,14 @@
 #' 
 #' @importFrom Rcpp evalCpp
 #' @export
-qubic <- function(i, N = FALSE, R = FALSE, F = FALSE, d = FALSE, D = FALSE, f = 0.85, k = 13, c = 0.90, o = 5000) {
+qubic <- function(i, N = FALSE, R = FALSE, F = FALSE, d = FALSE, D = FALSE, n = FALSE, f = 0.85, k = 13, c = 0.90, o = 5000) {
   vec <- c("./qubic", "-i", i)
   if(N) vec <- c(vec, "-N")
   if(R) vec <- c(vec, "-R")
   if(F) vec <- c(vec, "-F") 
   if(d) vec <- c(vec, "-d") 
   if(D) vec <- c(vec, "-D") 
+  if(n) vec <- c(vec, "-n")
   vec <- c(vec, "-f", as.character(f))
   vec <- c(vec, "-k", as.character(k))
   vec <- c(vec, "-c", as.character(c))
